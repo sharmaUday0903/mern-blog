a2cd3967e1e6a1725f7b58dc3321898afc03175b
sharmaUday0903 <113661887+sharmaUday0903@users.noreply.github.com>
2024-02-03 11:21:38 +0530

    final

--- api/index.js
+++ api/index.js
@@ -8,11 +8,10 @@ import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
-
 dotenv.config();
 
 mongoose
-  .connect("mongodb+srv://theuday965:P5wVVdkDnMtzjNJ@mern-blog.naoknei.mongodb.net/?retryWrites=true&w=majority")
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('MongoDb is connected');
   })
@@ -41,12 +40,7 @@ app.use(express.static(path.join(__dirname, '/client/dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
-// app.all('/*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "POST, GET");
-//   next();
-// });
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
