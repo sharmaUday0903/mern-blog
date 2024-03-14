18311a258ccb0b4080a4b10528581542e2560a40
sharmaUday0903 <113661887+sharmaUday0903@users.noreply.github.com>
2024-03-14 19:04:18 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -37,9 +37,13 @@ app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+app.get("/", (req, res) => {
+  res.json({"message": "Hello"})
+})
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
